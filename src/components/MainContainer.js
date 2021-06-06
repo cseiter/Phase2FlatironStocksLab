285a7cbc01e9d97a1c263f5b47dc863f5a523767
chris <cseiter@gmail.com>
2021-06-06 13:39:54 -0400

    set constants for useState

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React,{ useEffect,useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+
+  const [stocks, setStocks ] = useState(null)
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then(r => r.json())
+    .then(json => console.log(json))
+  }, []);
   return (
     <div>
       <SearchBar />
